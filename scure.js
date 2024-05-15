d43aa66071d17979bc1d44d63f5ae76a424be3ab
yehiamm19 <155308508+yehiamm19@users.noreply.github.com>
2024-05-15 14:40:19 +0300

    Create scure.js

---
+++ scure.js
@@ -0,0 +1,9 @@
+        document.addEventListener('contextmenu', function(e) {
+            e.preventDefault();
+        });
+
+        document.addEventListener('keydown', function(e) {
+            if (e.key === 'F12' || (e.ctrlKey && e.shiftKey && e.key === 'I')) {
+                e.preventDefault();
+            }
+        });
